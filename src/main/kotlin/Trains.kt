157204b79edb7d290165ce6b062189b530bdc38a
FlixsTHB <30578587+FlixsTHB@users.noreply.github.com>
2017-10-25 23:12:08 +0200

    Änderungen

--- src/main/kotlin/Trains.kt
+++ src/main/kotlin/Trains.kt
@@ -1,22 +1,23 @@
-<<<<<<< HEAD
+
 
 // Hätte jedem Zug eine Id gegeben und diese dann in der späteren scenario Funktion abrufen lassen.
 // Das mit dem Fahrplan fällt mir jedoch schwer, inwiefern man ausgeben kann wo und wann der Zug fahren soll...
 
 class Trains (val TrainId: MutableList<Int>, val wish: Boolean  ) {
-=======
+
 import java.util.Random
 
-class Trains (val TrainId: MutableList<1...12> = mutablelistof(Int), val WantsToDrive: MutableList<>  ){
+
+class Trains (val TrainId: MutableList<Int> = mutablelistof(1..12), val WantsToDrive: MutableList<>  ){
 
     var delayed: Boolean = false
-    var status: Boolean = Random()
+    var status: Boolean = random()
 
 
-    fun gettingDelayed() {
+    fun gettingDeleyed() {
         this.delayed = true
     }
->>>>>>> af74c89ab3b90fa9a6ae600e5a98716dd9484d63
+
 
 
 }
