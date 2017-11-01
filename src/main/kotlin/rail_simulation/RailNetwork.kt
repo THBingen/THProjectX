60961002f6f1f5231b21456780627d22aacc31b0
RobinGoetz <robin.goetz@th-bingen.de>
2017-11-01 23:55:03 +0100

    Klasse durch die Funktion remainingCapacity ergänzt

--- src/main/kotlin/rail_simulation/RailNetwork.kt
+++ src/main/kotlin/rail_simulation/RailNetwork.kt
@@ -8,6 +8,9 @@ class RailNetwork(val numberOfSegments: Int, val trainCapacity: Int) {
     //segments ist eine Liste aus 5 Segmenten mit je einer trainCapacity von max. 3 (aufgerufen durch die Klasse RailSegment)
     val segments = List(numberOfSegments, { RailSegment(trainCapacity) })
 
+    val totalCapacity = numberOfSegments * trainCapacity
+
+
     fun runSimulation(trains: List<Train>) {
 
         for (train in trains) {
@@ -19,6 +22,16 @@ class RailNetwork(val numberOfSegments: Int, val trainCapacity: Int) {
         for (segment in segments) {
             segment.checkCapacity()
         }
+    }
+
+    fun remainingCapacity(): Int {
+        var remainingCapacityTotal = 0
+        for (segment in segments) {
+            remainingCapacityTotal = segment.remainingCapacity() + remainingCapacityTotal
+
+
+        }
+        return remainingCapacityTotal
     }
 
 
